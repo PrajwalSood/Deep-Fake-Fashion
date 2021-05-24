dabaa8cfb868328a3a26c325d738e4f5e8a0945e
PrajwalSood <prajwalsood@gmail.com>
2021-05-24 20:01:50 +0530

    Added t-shirt changing in live video

--- live_video.py
+++ live_video.py
@@ -6,7 +6,7 @@ Created on Mon May 24 18:30:22 2021
 """
 
 from find_mask import masker, masker_np
-from utils import trim_masks, to_rle, display, convert_to_mask, crop_by_id
+from utils import trim_masks, to_rle, display, convert_to_mask, crop_by_id, convert_color
 import numpy as np
 import cv2
 import json
@@ -18,6 +18,8 @@ with open(".data/label_descriptions.json") as f:
     label_descriptions = json.load(f)
 
 vid = cv2.VideoCapture(0)
+vid.set(cv2.CAP_PROP_FRAME_WIDTH, 512)
+vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 512)
   
 while(True):
       
@@ -29,9 +31,11 @@ while(True):
         masks, rois = convert_to_mask(img, r)
         idx = list(r['class_ids']).index(2)
         res = crop_by_id(img, masks, idx)
-        cv2.imshow('frame', res)
+        final_image = convert_color(img, res, [255,0,0])
+        final_image = cv2.addWeighted(img,1,final_image,1,0)
+        cv2.imshow('frame', final_image)
     except:
-        cv2.imshow('frame', frame)
+        cv2.imshow('frame', cv2.resize(frame, (512,512)))
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
